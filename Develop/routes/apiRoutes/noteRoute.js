58ad50590859056185a0562bb19dc19e496018a6
Gromit <eversoryan@gmail.com>
2022-07-19 14:48:46 -0600

    Small changes

--- Develop/routes/apiRoutes/noteRoute.js
+++ Develop/routes/apiRoutes/noteRoute.js
@@ -1,6 +1,5 @@
 const uniqid = require('uniqid');
 const router = require('express').Router();
-const { notes } = require('../../db/db.json');
 const { viewNotes, createNote } = require('../../lib/notes');
 
 router.get('/api/notes', (req, res) => {
