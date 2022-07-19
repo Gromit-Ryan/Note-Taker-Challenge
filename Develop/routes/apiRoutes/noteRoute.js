519c54bd0795386ca132c6d68750aa0c7638cad0
Gromit <eversoryan@gmail.com>
2022-07-19 14:43:25 -0600

    Added route for get and post

---
+++ Develop/routes/apiRoutes/noteRoute.js
@@ -0,0 +1,20 @@
+const uniqid = require('uniqid');
+const router = require('express').Router();
+const { notes } = require('../../db/db.json');
+const { viewNotes, createNote } = require('../../lib/notes');
+
+router.get('/api/notes', (req, res) => {
+    viewNotes();
+})
+
+router.post('/api/notes', (req, res) => {
+    let note = {
+        id: uniqid(),
+        title: req.body.title,
+        text: req.body.text
+    }
+
+    createNote(note);
+});
+
+modules.export = router;
