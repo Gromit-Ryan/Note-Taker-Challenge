48515b0092fc0ed8f136a4647d39f544a00a97f5
Gromit <eversoryan@gmail.com>
2022-07-20 10:07:07 -0600

    Added server.js

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const apiRoutes = require('./routes/apiRoutes/index.js');
+const htmlRoutes = require('./routes/htmlRoutes/index.js');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.statis(path.join(__dirname, 'public')));
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Now listening on localhost:${PORT}`);
+})
